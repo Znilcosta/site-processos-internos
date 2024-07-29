6a53728c7696dfa662736b17cde5c542b3934a3d
Znilcosta <zenilson.costaf@hotmail.com>
2024-07-29 16:36:25 -0300

    Primeiro commit Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "../styles/Navbar.css";
+import logo from "../logo/logo-salsa.png";
+
+function Navbar() {
+  return (
+    <div className="navbar roboto-medium">
+      <div>
+        <img className="navbar-logo" src={logo} alt="Logo do Grupo Salsa" />
+      </div>
+      <div>
+        <ul className="navbar-list">
+          <li>
+            <a href="#">POP</a>
+          </li>
+          <li>
+            <a href="#">IT</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+        </ul>
+      </div>
+      <div>Login</div>
+    </div>
+  );
+}
+
+export default Navbar;
