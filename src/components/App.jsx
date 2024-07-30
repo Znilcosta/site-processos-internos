b54a0a64495b1114e5be784a3d175795b52b654d
Znilcosta <zenilson.costaf@hotmail.com>
2024-07-30 10:32:45 -0300

    Section sobre Missão, Visão e Valores

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 import Navbar from "./Navbar";
+import Company from "./Company";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <Company />
+    </div>
+  );
 }
 
 export default App;
