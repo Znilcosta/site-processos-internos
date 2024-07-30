d0ed34fff9ea71d7232ba7663aa46442a345a315
Znilcosta <zenilson.costaf@hotmail.com>
2024-07-30 11:54:23 -0300

    Slogan página inicial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Navbar from "./Navbar";
 import Company from "./Company";
+import Slogan from "./Slogan";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Company />
+      <Slogan />
     </div>
   );
 }
---
+++ src/components/Slogan.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "../styles/Slogan.css";
+
+function Slogan() {
+  return (
+    <section className="slogan">
+      <p className="visby">
+        O cliente tem um <strong>problema?</strong>
+      </p>
+      <p className="great-vibes-regular">Nós vamos resolver!</p>
+    </section>
+  );
+}
+
+export default Slogan;
